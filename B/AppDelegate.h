880a1997030ef2c263559be133764ace83f2c19b
xiaojieHuang <1430281855@qq.com>
2017-08-03 10:02:09 +0800

    Initial Commit

---
+++ B/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  B
+//
+//  Created by yiai_xiaojie on 2017/8/3.
+//  Copyright © 2017年 yiai_xiaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
